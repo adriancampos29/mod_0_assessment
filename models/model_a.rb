dc8aef4af33f3f57e831c7bb3ea3fcaf4db6a172
Adrian Campos <adriancampos2992@yahoo.com>
2021-10-22 13:28:11 -0700

    add airport variables

---
+++ models/model_a.rb
@@ -0,0 +1,11 @@
+AIRPORT = {
+  "name" => 'denver airport'
+  "closest_airport" => 'colorado springs',
+  "size_of_airport" => 'very big',
+  "average_amount_of_people_yearly" => 30000000,
+  "amount_of_stores_in_airport" => 170,
+  "do_big_planes_fly_there" => true,
+  "is_it_a_haunted_airport" => true,
+  "types_of_planes" => ['747', '737', '727'],
+  "popular_resturants" => ('rootdown', 'elways', 'chophouse')
+}
