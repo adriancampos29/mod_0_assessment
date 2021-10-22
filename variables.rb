ef193c4c651592a3a4f7f8641fd15b28eb8eed72
Adrian Campos <adriancampos2992@yahoo.com>
2021-10-22 13:20:49 -0700

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,9 @@
+famous_person = "ben_stiller"
+is_active = true
+famous_person = "brad_pitt"
+num_3 = 4
+num_9 = 10
+sum = num_9+num_3
+animals = ['dogs', 'cats', 'penguins']
+animals.shift
+famous_person.size
