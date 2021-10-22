6c32ed1b56c6e616302a8983d5f52536b763d0ca
Adrian Campos <adriancampos2992@yahoo.com>
2021-10-22 13:31:58 -0700

    add missing comma

--- models/model_a.rb
+++ models/model_a.rb
@@ -7,5 +7,5 @@ AIRPORT = {
   "do_big_planes_fly_there" => true,
   "is_it_a_haunted_airport" => true,
   "types_of_planes" => ['747', '737', '727'],
-  "popular_resturants" => ('rootdown', 'elways', 'chophouse')
+  "popular_resturants" => ('rootdown', 'elways', 'chophouse'),
 }
