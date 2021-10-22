4a689aaa23430a1bf8c01f7bc1600d88e3db2201
Adrian Campos <adriancampos2992@yahoo.com>
2021-10-22 13:37:07 -0700

    add vehicle varaibles

---
+++ models/model_b.rb
@@ -0,0 +1,11 @@
+VEHICLE = {
+  "name" => 'honda civic',
+  "model_of_car_owned" => 'sedan',
+  "manufactor_of_car" => 'honda',
+  "price" => 21000,
+  "amount_of_people_it_fits" => 5,
+  "is_spacious" => true,
+  "has_sunroof" => true,
+  "colors_available" => ['black', 'white', 'red', 'blue'],
+  "trim_levels" => ['lx', 'se', 'ex', 'ex-l'],
+}
